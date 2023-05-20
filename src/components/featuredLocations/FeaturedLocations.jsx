1d12315717718257001e4b547d705a963ea40b66
Rishav Sharma <rishavtosharma@gmail.com>
2023-05-20 23:40:33 +0530

    feat: :sparkles: Started integrating APIs in the frontend

--- src/components/featuredLocations/FeaturedLocations.jsx
+++ src/components/featuredLocations/FeaturedLocations.jsx
@@ -1,24 +1,42 @@
-import './featuredLocations.scss';
+import useFetch from "../../hooks/useFetch";
+import "./featuredLocations.scss";
 
 const Featured = ({ cityList }) => {
-  return (
-    <div className='flex justify-center w-full max-w-[1084px]'>
-      <div className='grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] grid gap-8 px-4 sm:px-8 lg:px-0 w-full'>
-        {
-          cityList.map((city, i) => {
-            return (
-              <div className="relative grid object-cover overflow-hidden rounded-md shadow-sm" key={i + 1}>
-                <img src={city.img} alt="" className="object-cover w-full h-[180px] col-start-1 col-end-2 row-start-1 row-end-2 aspect-square" />
-                <div className="mx-2 mt-1 text-white drop-shadow-[-1px_1px_1px_rgba(0,0,0,2)] col-start-1 col-end-2 row-start-1 row-end-2 ">
-                  <h1 className='m-0 text-2xl'>{city.name}</h1>
-                  <h2 className='m-0 text-xl'>{city.properties}</h2>
-                </div>
-              </div>
-            )
-          })
-        }
-      </div>
-    </div>
-  )
-}
-export default Featured
+	const { data, loading, error } = useFetch(
+		"/api/hotels/countByCity?cities=Dalhousie,Pangi,Chamba,Dharamshala,Kasol,Malana,Manali,Shimla,Lahaul-Spiti,Kaza"
+	);
+	return (
+		<div className="flex justify-center w-full max-w-[1084px]">
+			<div className="grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] grid gap-8 px-4 sm:px-8 lg:px-0 w-full">
+				{loading
+					? cityList.map((city, i) => {
+							return (
+								<div
+									key={i + 1}
+									className="flex justify-center items-center w-auto h-[180px] bg-gray-200 animate-pulse rounded-md shadow-sm">
+									{/* Loading... */}
+								</div>
+							);
+					  })
+					: cityList.map((city, i) => {
+							return (
+								<div
+									className="relative grid object-cover overflow-hidden rounded-md shadow-sm"
+									key={i + 1}>
+									<img
+										src={city.img}
+										alt=""
+										className="object-cover w-full h-[180px] col-start-1 col-end-2 row-start-1 row-end-2 aspect-square"
+									/>
+									<div className="mx-2 mt-1 text-white drop-shadow-[-1px_1px_1px_rgba(0,0,0,2)] col-start-1 col-end-2 row-start-1 row-end-2 ">
+										<h1 className="m-0 text-2xl">{city.name}</h1>
+										<h2 className="m-0 text-xl">{city.properties}</h2>
+									</div>
+								</div>
+							);
+					  })}
+			</div>
+		</div>
+	);
+};
+export default Featured;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,179 +1,247 @@
-import "./header.scss"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBed, faCalendarDays, faCar, faEarthAsia, faPerson, faPlane, faTaxi, faUser } from '@fortawesome/free-solid-svg-icons'
+import "./header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faBed,
+	faCalendarDays,
+	faCar,
+	faEarthAsia,
+	faPerson,
+	faPlane,
+	faTaxi,
+	faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import { DateRange } from "react-date-range";
-import { useEffect, useRef, useState } from 'react';
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
+import { useEffect, useRef, useState } from "react";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 import { format } from "date-fns/esm";
 import { useNavigate } from "react-router-dom";
 
 const Header = ({ type }) => {
+	const menuRef = useRef();
 
-    const menuRef = useRef()
+	// Destination selection
+	const [destination, setDestination] = useState("");
 
-    // Destination selection
-    const [destination, setDestination] = useState("");
+	// Booking dates selection
+	const [openPopCalendar, setOpenPopCalendar] = useState(false);
+	const [date, setDate] = useState([
+		{
+			startDate: new Date(),
+			endDate: new Date(),
+			key: "selection",
+		},
+	]);
 
-    // Booking dates selection
-    const [openPopCalendar, setOpenPopCalendar] = useState(false);
-    const [date, setDate] = useState([
-        {
-            startDate: new Date(),
-            endDate: new Date(),
-            key: 'selection'
-        }
-    ]);
+	// Room and number of people selection
+	const [openOptions, setOpenOptions] = useState(false);
+	const [options, setOptions] = useState({
+		adult: 1,
+		children: 0,
+		room: 1,
+	});
 
-    // Room and number of people selection
-    const [openOptions, setOpenOptions] = useState(false);
-    const [options, setOptions] = useState(
-        {
-            adult: 1,
-            children: 0,
-            room: 1,
-        },
-    );
+	//Input refs
+	const locationInputRef = useRef();
+	const dateInputRef = useRef();
+	const adultInputRef = useRef();
+	const childrenInputRef = useRef();
+	const roomInputRef = useRef();
 
-    //Input refs
-    const locationInputRef = useRef()
-    const dateInputRef = useRef()
-    const adultInputRef = useRef()
-    const childrenInputRef = useRef()
-    const roomInputRef = useRef()
+	// Navigating to hotels page
+	const navigate = useNavigate();
+	const inputRef = useRef();
+	const handleSearch = (e) => {
+		try {
+			if (
+				locationInputRef.current.value === "" &&
+				locationInputRef.current.value === null &&
+				dateInputRef.current.value === ""
+			) {
+				alert("Please fill all inputs to search the hotels!");
+			} else {
+				navigate("/hotels", { state: { destination, date, options } });
+				console.log("Location input ref: " + locationInputRef.current.value);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
+	const handleOption = (name, logic) => {
+		setOptions((prev) => {
+			return {
+				...prev,
+				[name]: logic === "d" ? options[name] - 1 : options[name] + 1,
+			};
+		});
+	};
 
-    // Navigating to hotels page
-    const navigate = useNavigate();
-    const inputRef = useRef();
-    const handleSearch = (e) => {
-        try {
-            if (locationInputRef.current.value === ""
-                && locationInputRef.current.value === null && dateInputRef.current.value === ""
-            ) {
-                alert("Please fill all inputs to search the hotels!")
-            }
-            else {
-                navigate("/hotels", { state: { destination, date, options } })
-                console.log("Location input ref: " + locationInputRef.current.value)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
+	return (
+		<div className="header">
+			<div className="headerContainer">
+				<div
+					className={
+						type === "hotels" ? "headerList hotelsMode" : "headerList"
+					}>
+					<div className="headerListItem active">
+						<FontAwesomeIcon icon={faBed} />
+						<span>Stays</span>
+					</div>
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faPlane} />
+						<span>Flights</span>
+					</div>
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faCar} />
+						<span>Car Rentals</span>
+					</div>
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faEarthAsia} />
+						<span>Attractions</span>
+					</div>
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faTaxi} />
+						<span>Airport Taxis</span>
+					</div>
+				</div>
 
-    const handleOption = (name, logic) => {
-        setOptions((prev) => {
-            return {
-                ...prev, [name]: logic === "d" ? options[name] - 1 : options[name] + 1
-            }
-        })
-    }
+				{type != "hotels" && (
+					<>
+						<div className="headerContent">
+							<h2>Travel with burning less cash!</h2>
+							<p>
+								Want to get rewarded for traveling? Get an instant discount of
+								10% when you create your account on HappyBooking.com
+							</p>
+							<button className="headerBtn">Register</button>
+						</div>
+						<div className="headerSearch">
+							<div className="headerSearchContainer">
+								<div className="headerSearchItem">
+									<FontAwesomeIcon icon={faBed} className="icon" />
+									<input
+										className="cursor-text headerSearchInput"
+										ref={locationInputRef}
+										onChange={(e) => setDestination(e.target.value)}
+										type="text"
+										placeholder="Where are you going?"
+									/>
+								</div>
+								<div className="headerSearchItem">
+									<FontAwesomeIcon icon={faCalendarDays} className="icon" />
+									<div ref={menuRef}>
+										<span
+											className="cursor-pointer select-none headerSearchText calendarPopInput "
+											ref={dateInputRef}
+											onClick={() => setOpenPopCalendar(!openPopCalendar)}>
+											{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(
+												date[0].endDate,
+												"dd/MM/yyyy"
+											)}`}
+										</span>
+										{openPopCalendar && (
+											<DateRange
+												editableDateInputs={true}
+												onChange={(item) => setDate([item.selection])}
+												moveRangeOnFirstSelection={false}
+												ranges={date}
+												minDate={new Date()}
+												ref={inputRef}
+												className="datePopCalendar"
+											/>
+										)}
+									</div>
+								</div>
+								<div className="headerSearchItem">
+									<FontAwesomeIcon icon={faPerson} className="icon" />
+									<div ref={menuRef}>
+										<span
+											className="cursor-pointer select-none headerSearchText optionsPopInput"
+											onClick={() =>
+												setOpenOptions(!openOptions)
+											}>{` ${options.adult} adult · ${options.children} children · ${options.room} room`}</span>
+										{openOptions && (
+											<div className="headerOptions">
+												<div className="optionItem">
+													<span className="adult">Adult</span>
+													<div className="numbers">
+														<button
+															disabled={options.adult <= 1}
+															onClick={() => handleOption("adult", "d")}
+															className="decreaseNumber">
+															{" "}
+															-{" "}
+														</button>
+														<span className="number" ref={adultInputRef}>
+															{" "}
+															{`${options.adult}`}{" "}
+														</span>
+														<button
+															onClick={() => handleOption("adult", "i")}
+															className="increaseNumber">
+															+
+														</button>
+													</div>
+												</div>
+												<div className="optionItem">
+													<span className="children">Children</span>
+													<div className="numbers">
+														<button
+															disabled={options.children <= 0}
+															onClick={() => handleOption("children", "d")}
+															className="decreaseNumber">
+															{" "}
+															-{" "}
+														</button>
+														<span className="number" ref={childrenInputRef}>
+															{" "}
+															{`${options.children}`}
+														</span>
+														<button
+															onClick={() => handleOption("children", "i")}
+															className="increaseNumber">
+															+
+														</button>
+													</div>
+												</div>
+												<div className="optionItem">
+													<span className="room">Room</span>
+													<div className="numbers">
+														<button
+															disabled={options.room <= 1}
+															onClick={() => handleOption("room", "d")}
+															className="decreaseNumber">
+															{" "}
+															-{" "}
+														</button>
+														<span className="number" ref={roomInputRef}>
+															{" "}
+															{`${options.room}`}
+														</span>
+														<button
+															onClick={() => handleOption("room", "i")}
+															className="increaseNumber">
+															+
+														</button>
+													</div>
+												</div>
+											</div>
+										)}
+									</div>
+								</div>
+								<div className="headerBtnContainer">
+									<button className="headerBtn" onClick={handleSearch}>
+										Search
+									</button>
+								</div>
+							</div>
+						</div>
+					</>
+				)}
+			</div>
+		</div>
+	);
+};
 
-    return (
-        <div className="header">
-            <div className="headerContainer">
-                <div className={type === "hotels" ? "headerList hotelsMode" : "headerList"}>
-                    <div className="headerListItem active">
-                        <FontAwesomeIcon icon={faBed} />
-                        <span>Stays</span>
-                    </div>
-                    <div className="headerListItem">
-                        <FontAwesomeIcon icon={faPlane} />
-                        <span>Flights</span>
-                    </div>
-                    <div className="headerListItem">
-                        <FontAwesomeIcon icon={faCar} />
-                        <span>Car Rentals</span>
-                    </div>
-                    <div className="headerListItem">
-                        <FontAwesomeIcon icon={faEarthAsia} />
-                        <span>Attractions</span>
-                    </div>
-                    <div className="headerListItem">
-                        <FontAwesomeIcon icon={faTaxi} />
-                        <span>Airport Taxis</span>
-                    </div>
-                </div>
-
-                {type != "hotels" &&
-                    <>
-                        <div className="headerContent">
-                            <h2>Travel with burning less cash!</h2>
-                            <p>Want to get rewarded for traveling? Get an instant discount of 10% when you create your account on HappyBooking.com</p>
-                            <button className="headerBtn">Register</button>
-                        </div>
-                        <div className="headerSearch">
-                            <div className="headerSearchContainer">
-                                <div className="headerSearchItem">
-                                    <FontAwesomeIcon icon={faBed} className="icon" />
-                                    <input className="headerSearchInput"
-                                        ref={locationInputRef}
-                                        onChange={(e) => setDestination(e.target.value)}
-                                        type="text"
-                                        placeholder="Where are you going?"
-                                    />
-                                </div>
-                                <div className="headerSearchItem" >
-                                    <FontAwesomeIcon icon={faCalendarDays} className="icon" />
-                                    <div ref={menuRef} >
-                                        <span className="headerSearchText calendarPopInput" ref={dateInputRef} onClick={() => setOpenPopCalendar(!openPopCalendar)}>
-                                            {`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(date[0].endDate, "dd/MM/yyyy")}`}
-                                        </span>
-                                        {openPopCalendar &&
-                                            <DateRange
-                                                editableDateInputs={true}
-                                                onChange={item => setDate([item.selection])}
-                                                moveRangeOnFirstSelection={false}
-                                                ranges={date}
-                                                minDate={new Date()}
-                                                ref={inputRef}
-                                                className="datePopCalendar"
-                                            />
-                                        }
-                                    </div>
-                                </div>
-                                <div className="headerSearchItem">
-                                    <FontAwesomeIcon icon={faPerson} className="icon" />
-                                    <div ref={menuRef}>
-                                        <span className="headerSearchText optionsPopInput" onClick={() => setOpenOptions(!openOptions)} >{` ${options.adult} adult · ${options.children} children · ${options.room} room`}</span>
-                                        {openOptions && <div className="headerOptions" >
-                                            <div className="optionItem">
-                                                <span className="adult">Adult</span>
-                                                <div className="numbers">
-                                                    <button disabled={options.adult <= 1} onClick={() => handleOption("adult", "d")} className="decreaseNumber"> - </button>
-                                                    <span className="number" ref={adultInputRef}> {`${options.adult}`} </span>
-                                                    <button onClick={() => handleOption("adult", "i")} className="increaseNumber">+</button>
-                                                </div>
-                                            </div>
-                                            <div className="optionItem">
-                                                <span className="children">Children</span>
-                                                <div className="numbers">
-                                                    <button disabled={options.children <= 0} onClick={() => handleOption("children", "d")} className="decreaseNumber"> - </button>
-                                                    <span className="number" ref={childrenInputRef}> {`${options.children}`}</span>
-                                                    <button onClick={() => handleOption("children", "i")} className="increaseNumber">+</button>
-                                                </div>
-                                            </div>
-                                            <div className="optionItem">
-                                                <span className="room">Room</span>
-                                                <div className="numbers">
-                                                    <button disabled={options.room <= 1} onClick={() => handleOption("room", "d")} className="decreaseNumber"> - </button>
-                                                    <span className="number" ref={roomInputRef}>  {`${options.room}`}</span>
-                                                    <button onClick={() => handleOption("room", "i")} className="increaseNumber">+</button>
-                                                </div>
-                                            </div>
-                                        </div>}
-                                    </div>
-                                </div>
-                                <div className="headerBtnContainer">
-                                    <button className="headerBtn" onClick={handleSearch}>Search</button>
-                                </div>
-                            </div>
-                        </div>
-                    </>}
-            </div>
-        </div>
-    )
-}
-
-export default Header
+export default Header;
