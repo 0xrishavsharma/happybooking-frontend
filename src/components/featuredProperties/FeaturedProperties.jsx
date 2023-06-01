9cf03f3ba101d0d2aecb181ad765f01119ad94f3
Rishav Sharma <rishavtosharma@gmail.com>
2023-06-01 11:13:48 +0530

    feat: :sparkles: replacing dummy data with actual database and integrating apis

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,34 +1,49 @@
-import "./featuredProperties.scss"
-import { featuredPropertiesData } from "./../../utils/featuredProperties"
+import "./featuredProperties.scss";
+import { featuredPropertiesData } from "./../../utils/featuredProperties";
+import useFetch from "../../hooks/useFetch";
 
 const FeaturedProperties = () => {
-    return (
-        <div className='fp'>
-            <h1 className="px-4 title sm:px-8 lg:px-0">Trending properties</h1>
-            <div className="px-4 card fpWrapper sm:px-8 lg:px-0">
-                {
-                    featuredPropertiesData.slice(0, 4).map((property) => {
-                        const { id, img, hotelName, location, price, rating, review } = property
-                        return (
-                            <div className="fpItem" key={id + 1}>
-                                <img
-                                    src={img}
-                                    alt=""
-                                    className="fpImg w-full h-[200px] object-cover min-w-[184px] "
-                                />
-                                <h3 className="my-0 fpName">{hotelName}</h3>
-                                <h4 className="my-0 fpCity">{location}</h4>
-                                <span className="fpPrice">{price}</span>
-                                <div className="flex gap-2 rating">
-                                    <button className={`bg-[color:var(--primary-color)] font-bold px-2 py-[1.5px] text-white border-none rounded-sm`}>{rating}</button>
-                                    <span>{review}</span>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
-export default FeaturedProperties
+	const { data, loading, error } = useFetch("/api/hotels?featured=true");
+	let sortedData = data?.sort((a, b) => b.rating - a.rating);
+	return (
+		<div className="fp">
+			<h1 className="px-4 title sm:px-8 lg:px-0">Trending properties</h1>
+			<div className="px-4 card fpWrapper sm:px-8 lg:px-0">
+				{featuredPropertiesData.slice(0, 4).map((property, i) => {
+					const { id, img, hotelName, location, price, rating, review } =
+						property;
+					return (
+						<div className="fpItem" key={id + 1}>
+							<img
+								src={img}
+								alt=""
+								className="fpImg w-full h-[200px] object-cover min-w-[184px] "
+							/>
+							<h3 className="my-0">
+								<span className="text-xl font-bold">
+									{sortedData ? sortedData[i]?.name : hotelName}
+								</span>
+								<span className="text-sm opacity-80">
+									({sortedData ? sortedData[i]?.type : type})
+								</span>
+							</h3>
+							<h4 className="my-0 fpCity">{sortedData[i]?.city}</h4>
+							<span className="fpPrice">
+								Starting from ₹
+								{sortedData ? sortedData[i]?.cheapestPrice : price}
+							</span>
+							<div className="flex gap-2 rating">
+								<button
+									className={`bg-[color:var(--primary-color)] font-bold px-2 py-[1.5px] text-white border-none rounded-sm`}>
+									{sortedData ? sortedData[i]?.rating : rating}
+								</button>
+								<span>{review}</span>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+export default FeaturedProperties;
--- src/components/propertiesList/PropertiesList.jsx
+++ src/components/propertiesList/PropertiesList.jsx
@@ -1,30 +1,49 @@
 import "./propertiesList.scss";
 import { properties } from "./../../utils/propertiesList";
+import useFetch from "../../hooks/useFetch";
 
 const PropertiesList = () => {
-    return (
-        <div className="propertiesList flex flex-col gap-6 w-full justify-between] ">
-            <h1 className="px-4 mb-0 text-2xl font-extrabold title sm:px-8 lg:px-0">Browse by property type</h1>
-            <div className="flex w-full gap-8 px-4 overflow-x-scroll sm:px-8 lg:px-0 card lg:overflow-hidden">
-                {
-                    properties.map((property, i) => {
-                        const { id, img, propertyName, total } = property;
-                        return (
-                            <div className="flex-1 px-4 cursor-pointer sm:px-8 lg:px-0" key={id + 1}>
-                                <img
-                                    src={img}
-                                    alt=""
-                                    className="min-w-[160px] h-[140px] w-full object-cover flex-1 rounded-sm "
-                                />
-                                <h3 className="mt-3 mb-1 text-xl font-extrabold">{propertyName}</h3>
-                                <p className="text-sm">{total}</p>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+	const { data, loading, error } = useFetch("/api/hotels/countByType");
+	return (
+		<div className="propertiesList flex flex-col gap-6 w-full justify-between] ">
+			<h1 className="px-4 mb-0 text-2xl font-extrabold title sm:px-8 lg:px-0">
+				Browse by property type
+			</h1>
+			<div className="flex w-full gap-8 px-4 overflow-x-scroll sm:px-8 lg:px-0 card lg:overflow-hidden">
+				{loading
+					? properties.map((city, i) => {
+							return (
+								<div
+									key={i + 1}
+									className="flex flex-1 justify-center items-center h-[180px] bg-gray-200 animate-pulse rounded-md shadow-sm">
+									{/* Loading... */}
+								</div>
+							);
+					  })
+					: properties.map((property, i) => {
+							const { id, img, propertyName, total } = property;
+							return (
+								<div
+									className="flex-1 px-4 cursor-pointer sm:px-8 lg:px-0"
+									key={id + 1}>
+									<img
+										src={img}
+										alt=""
+										className="min-w-[160px] h-[140px] w-full object-cover flex-1 rounded-sm "
+									/>
+									<h3 className="mt-3 mb-1 text-xl font-extrabold capitalize">
+										{data[i]?.name}
+									</h3>
+									<p className="flex gap-1 text-sm">
+										<span>{data[i]?.value} </span>
+										<span>{data[i]?.name}s </span>
+									</p>
+								</div>
+							);
+					  })}
+			</div>
+		</div>
+	);
+};
 
-export default PropertiesList
+export default PropertiesList;
