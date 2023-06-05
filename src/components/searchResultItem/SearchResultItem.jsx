8c8c4f85992f141b9d759c625c233a0f16400416
Rishav Sharma <rishavtosharma@gmail.com>
2023-06-06 01:35:08 +0530

    feat: started working on home page search functionality

--- src/components/searchResultItem/SearchResultItem.jsx
+++ src/components/searchResultItem/SearchResultItem.jsx
@@ -1,50 +1,68 @@
-import React from 'react';
+import React from "react";
 import "./searchResultItem.scss";
+import { Link } from "react-router-dom";
 
-const SearchItem = () => {
-    return (
-        <div className='searchResultItem'>
-            <div className='first'>
-                <img src="https://cf.bstatic.com/xdata/images/hotel/square200/338187202.webp?k=ac24232b812e6ce84fb3bf46dbf613bcc75a7beb1ae46d2238ea1f515d54a2f7&amp;o=&amp;s=1"
-                    alt="FabHotel Misty Hills Nehru Kund"
-                    width="200" height="200"
-                    data-testid="image" class="b8b0793b0e"
-                    className='rounded-sm'
-                />
-            </div>
-            <div className='second'>
-                <h2>Tower Street Apartments</h2>
-                <span>500m from center</span>
-                <span className="taxiOp">Free airport taxi</span>
-                <div className="details">
-                    <span>
-                        <b>Studio apartment with Air Conditioning</b>
-                        <p>Entire Studio · 1 Bathroom · 21m&#xb2; 1 Full bed</p>
-                    </span>
-                    <p className="cancelOp">Free cancellation</p>
-                    <p className='cancelLaterOp'>You can cancel later, so lock in this great price today!</p>
-                </div>
-            </div>
-            <div className='third'>
-                <div className="rating">
-                    <div className="left">
-                        <h4>Good</h4>
-                        <span>12 reviews</span>
-                    </div>
-                    <div className="right">
-                        7.7
-                    </div>
-                </div>
-                <span className='hotelComfort'>Comfort 8.2</span>
-                <div className="bottom">
-                    <span className="daysPerson">7 nights, 2 adults </span>
-                    <h3>₹ 12,506</h3>
-                    <span className="hotelTaxes">+ ₹1,501 taxes and charges</span>
-                    <button>See availability</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+const SearchItem = ({ item }) => {
+	console.log("item", item?.photos?.length > 0);
+	return (
+		<div className="searchResultItem">
+			<div className="first">
+				<img
+					// src={
+					// 	item?.photos?.length > 0
+					// 		? item.photos[0]
+					// 		: "https://cf.bstatic.com/xdata/images/hotel/square200/338187202.webp?k=ac24232b812e6ce84fb3bf46dbf613bcc75a7beb1ae46d2238ea1f515d54a2f7&amp;o=&amp;s=1"
+					// }
+					src="https://cf.bstatic.com/xdata/images/hotel/square200/338187202.webp?k=ac24232b812e6ce84fb3bf46dbf613bcc75a7beb1ae46d2238ea1f515d54a2f7&amp;o=&amp;s=1"
+					alt={item ? item.name : "Hotel/Resort/Apartment photo"}
+					width="200"
+					height="200"
+					data-testid="image"
+					class="b8b0793b0e"
+					className="rounded-sm"
+				/>
+			</div>
+			<div className="second">
+				<h2>{item?.name}</h2>
+				<span>{item.distance}</span>
+				<span className="taxiOp">Free airport taxi</span>
+				<div className="details">
+					<span>
+						{/* <b>Studio apartment with Air Conditioning</b>
+						<p>Entire Studio · 1 Bathroom · 21m&#xb2; 1 Full bed</p> */}
+						{item?.description}
+					</span>
+					<p className="cancelOp">Free cancellation</p>
+					<p className="cancelLaterOp">
+						Cancel anytime, so lock in this great deal today!
+					</p>
+				</div>
+			</div>
+			<div className="third">
+				<div className="rating">
+					{item.rating && (
+						<>
+							<div className="left">
+								<h4>Good</h4>
+								<span>12 reviews</span>
+							</div>
+							<div className="right">{item.rating}</div>
+						</>
+					)}
+				</div>
+				<span className="hotelComfort">Comfort 8.2</span>
+				<div className="bottom">
+					<span className="daysPerson">7 nights, 2 adults </span>
+					<h3>₹ {item?.cheapestPrice}</h3>
+					{/* <span className="hotelTaxes">+ ₹1,501 taxes and charges</span> */}
+					<span className="hotelTaxes">Includes taxes and fees</span>
+					<Link to={`/hotels/${item._id}`}>
+						<button>See availability</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default SearchItem
+export default SearchItem;
--- src/pages/hotels/Hotels.jsx
+++ src/pages/hotels/Hotels.jsx
@@ -1,4 +1,7 @@
-import { faCircleQuestion, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+	faCircleQuestion,
+	faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getDay, format } from "date-fns";
 import { useRef } from "react";
@@ -9,127 +12,157 @@ import { useLocation } from "react-router-dom";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import SearchResultItem from "../../components/searchResultItem/SearchResultItem";
+import useFetch from "../../hooks/useFetch";
 import "./hotels.scss";
 
 const Hotels = () => {
+	const location = useLocation();
 
-  const location = useLocation();
+	const [destination, setDestination] = useState(location.state.destination);
+	const [date, setDate] = useState(location.state.date);
+	const [openDate, setOpenDate] = useState(false);
+	const [options, setOptions] = useState(location.state.options);
 
-  const [destination, setDestination] = useState(location.state.destination);
-  const [date, setDate] = useState(location.state.date);
-  console.log("Date data:", date)
-  const [openDate, setOpenDate] = useState(false);
-  const [options, setOptions] = useState(location.state.options);
+	const formattedDestination =
+		destination.charAt(0).toUpperCase() + destination.slice(1).toLowerCase();
+	const { data, error, loading, refetch } = useFetch(
+		`/api/hotels?city=${formattedDestination}`
+	);
 
-  // Closing the date popup when clicking outside of the popup
-  const datePopUpRef = useRef();
-  // useEffect(() => {
-  //   document.addEventListener('mousedown', (event) => {
-  //     if (!datePopUpRef.current.contains(event.target)) {
-  //       setOpenDate(false);
-  //     }
-  //   })
-  // }, [])
+	// Closing the date popup when clicking outside of the popup
+	const datePopUpRef = useRef();
+	// useEffect(() => {
+	//   document.addEventListener('mousedown', (event) => {
+	//     if (!datePopUpRef.current.contains(event.target)) {
+	//       setOpenDate(false);
+	//     }
+	//   })
+	// }, [])
 
-  return (
-    <div className="hotels">
-      <Navbar />
-      <Header type="hotels" />
-      <div className="flex justify-center hotelsContainer">
-        <div className="hotelsWrapper max-w-[1084px] flex flex-col md:flex-row">
-          <div className="hotelSearch lg:sticky">
-            <h1 className="title">Search</h1>
-            <div className="searchItem">
-              <p>Destination/Property Name:</p>
-              <div className="input">
-                <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
-                <input type="text" placeholder="Place/Property Name" value={destination} onChange={(e) => setDestination(e.target.value)} />
-              </div>
-            </div>
-            <div className="searchItem">
-              <p>Check-in to Check-out Date</p>
-              <div className="input" ref={datePopUpRef} onClick={(e) => setOpenDate(!openDate)}>
-                <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
-                <span>
-                  {`${format(date[0].startDate, "dd/MM/yyyy")}`} to {`${format(date[0].endDate, "dd/MM/yyyy")}`}
-                </span>
-                {openDate && (<DateRange
-                  onChange={(item) => setDate([item.selection])}
-                  minDate={new Date()}
-                  ranges={date}
-                  className="date"
-                  fixedHeight="240"
-                />)
-                }
-              </div>
-            </div>
-            <div className="searchItem options">
-              <p>Options</p>
-              <div className="optionsContainer">
-                <div className="option">
-                  <span>Min price <small>(per night)</small> </span>
-                  <input type="number" />
-                </div>
-                <div className="option">
-                  <span>Max price <small>(per night)</small> </span>
-                  <input type="number" />
-                </div>
-                <div className="option">
-                  <span>Adult</span>
-                  <input type="number" min={1} placeholder={options.adult} />
-                </div>
-                <div className="option">
-                  <span>Children</span>
-                  <input type="number" min={0} placeholder={options.children} />
-                </div>
-                <div className="option">
-                  <span>Room</span>
-                  <input type="number" min={1} placeholder={options.room} />
-                </div>
-              </div>
-            </div>
-            <div className="checkbox">
-              <div className="flex gap-2 checkboxWrapper">
-                <input type="checkbox" name="" id="" />
-                <span>I'm traveling for work</span>
-              </div>
-              <FontAwesomeIcon icon={faCircleQuestion} className="icon" />
-            </div>
-            <button type="submit" className="searchBtn">Search</button>
-          </div>
-          <div className="hotelResultContainer">
-            <div className="hrHeading">
-              <h2>{destination ? destination : "Manali"}: 372 properties found</h2>
-              <div className="mapBtn">
-                <button>Show on map</button>
-              </div>
-            </div>
-            <div className="optionsBar">
-              <span className="active">Top picks for long stays</span>
-              <span>Homes & apartments first</span>
-              <span>Price (lowest first)</span>
-              <span>Best reviewed & lowest price</span>
-              <span>
-                <FontAwesomeIcon icon={faCircleQuestion} />
-              </span>
-            </div>
-            <div className="hotelResults">
-              <SearchResultItem />
-              <SearchResultItem />
-              <SearchResultItem />
-              <SearchResultItem />
-              <SearchResultItem />
-              <SearchResultItem />
-              <SearchResultItem />
-              <SearchResultItem />
-              <SearchResultItem />
-              <SearchResultItem />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="hotels">
+			<Navbar />
+			<Header type="hotels" />
+			<div className="flex justify-center hotelsContainer">
+				<div className="hotelsWrapper max-w-[1084px] flex flex-col md:flex-row">
+					<div className="hotelSearch lg:sticky">
+						<h1 className="title">Search</h1>
+						<div className="searchItem">
+							<p>Destination/Property Name:</p>
+							<div className="input">
+								<FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
+								<input
+									type="text"
+									placeholder="Place/Property Name"
+									value={formattedDestination}
+									onChange={(e) => setDestination(e.target.value)}
+								/>
+							</div>
+						</div>
+						<div className="searchItem">
+							<p>Check-in to Check-out Date</p>
+							<div
+								className="input"
+								ref={datePopUpRef}
+								onClick={(e) => setOpenDate(!openDate)}>
+								<FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
+								<span>
+									{`${format(date[0].startDate, "dd/MM/yyyy")}`} to{" "}
+									{`${format(date[0].endDate, "dd/MM/yyyy")}`}
+								</span>
+								{openDate && (
+									<DateRange
+										onChange={(item) => setDate([item.selection])}
+										minDate={new Date()}
+										ranges={date}
+										className="date"
+										fixedHeight="240"
+									/>
+								)}
+							</div>
+						</div>
+						<div className="searchItem options">
+							<p>Options</p>
+							<div className="optionsContainer">
+								<div className="option">
+									<span>
+										Min price <small>(per night)</small>{" "}
+									</span>
+									<input type="number" />
+								</div>
+								<div className="option">
+									<span>
+										Max price <small>(per night)</small>{" "}
+									</span>
+									<input type="number" />
+								</div>
+								<div className="option">
+									<span>Adult</span>
+									<input type="number" min={1} placeholder={options.adult} />
+								</div>
+								<div className="option">
+									<span>Children</span>
+									<input type="number" min={0} placeholder={options.children} />
+								</div>
+								<div className="option">
+									<span>Room</span>
+									<input type="number" min={1} placeholder={options.room} />
+								</div>
+							</div>
+						</div>
+						<div className="checkbox">
+							<div className="flex gap-2 checkboxWrapper">
+								<input type="checkbox" name="" id="" />
+								<span>I'm traveling for work</span>
+							</div>
+							<FontAwesomeIcon icon={faCircleQuestion} className="icon" />
+						</div>
+						<button type="submit" className="searchBtn">
+							Search
+						</button>
+					</div>
+					<div className="hotelResultContainer">
+						<div className="hrHeading">
+							<h2>
+								{formattedDestination &&
+									`${formattedDestination} : 372 properties found`}
+							</h2>
+							<div className="mapBtn">
+								<button>Show on map</button>
+							</div>
+						</div>
+						<div className="optionsBar">
+							<span className="active">Top picks for long stays</span>
+							<span>Homes & apartments first</span>
+							<span>Price (lowest first)</span>
+							<span>Best reviewed & lowest price</span>
+							<span>
+								<FontAwesomeIcon icon={faCircleQuestion} />
+							</span>
+						</div>
+						<div className="hotelResults">
+							{console.log("Loading: ", loading)}
+							{/* {loading ? (
+								data.map((item, i) => {
+									return (
+										<div
+											key={i + 1}
+											className="flex flex-1 justify-center items-center h-[180px] bg-gray-200 animate-pulse rounded-md shadow-sm"></div>
+									);
+								})
+							) : ( */}
+							<div>
+								{data.map((item, i) => (
+									<SearchResultItem item={item} key={i} />
+								))}
+							</div>
+							{/* )} */}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Hotels;
