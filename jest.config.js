cfa3a70b199585a3db9380ac84b9687b5f146e46
alek1339 <aleksandermihaylov@gmail.com>
2023-06-07 11:28:14 +0300

    Configure jest
    Installed and added configuration for jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(scss)$': '<rootDir>/_mocks/styleMock.js',
+    '\\.(css)$': '<rootDir>/_mocks/styleMock.js',
+    '\\.(svg)$': '<rootDir>/_mocks/fileMock.js',
+  },
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!axios|date-fns/esm)',
+  ],
+};
